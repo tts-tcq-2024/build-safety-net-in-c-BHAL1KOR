ed44df3480914f74fa14ee1e5da329dd63c2d018
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-02 19:32:47 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -8,6 +8,7 @@
 
 #define MAX_CODE_LENGTH 4
 
+
 char getSoundexCode(char c) {
     static const char soundexTable[26] = {
         '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
