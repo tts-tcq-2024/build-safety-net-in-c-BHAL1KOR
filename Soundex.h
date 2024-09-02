f5a81d4b69675f3d5ef601b850e141e8078d24f4
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-02 17:35:02 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -19,33 +19,36 @@ char getSoundexCode(char c) {
     return '0'; // For non-alphabetic characters
 }
 
-void generateSoundex(const char *name, char *soundex) {
-    if (name == NULL || soundex == NULL) {
-        return;
+void initializeSoundex(char *soundex, char firstLetter) {
+    soundex[0] = toupper(firstLetter);
+    for (int i = 1; i < MAX_CODE_LENGTH; i++) {
+        soundex[i] = '0';
     }
+    soundex[MAX_CODE_LENGTH] = '\0';
+}
 
-    int len = strlen(name);
-    if (len == 0) {
-        soundex[0] = '\0';
-        return;
-    }
-
-    soundex[0] = toupper(name[0]);
+void processCharacters(const char *name, char *soundex) {
     int sIndex = 1;
     char lastCode = getSoundexCode(soundex[0]);
 
-    for (int i = 1; i < len && sIndex < MAX_CODE_LENGTH; i++) {
+    for (int i = 1; name[i] != '\0' && sIndex < MAX_CODE_LENGTH; i++) {
         char code = getSoundexCode(name[i]);
         if (code != '0' && code != lastCode) {
             soundex[sIndex++] = code;
             lastCode = code;
         }
     }
+}
 
-    while (sIndex < MAX_CODE_LENGTH) {
-        soundex[sIndex++] = '0';
+void generateSoundex(const char *name, char *soundex) {
+    if (name == NULL || soundex == NULL || name[0] == '\0') {
+        if (soundex != NULL) {
+            soundex[0] = '\0';
+        }
+        return;
     }
 
-    soundex[MAX_CODE_LENGTH] = '\0';
+    initializeSoundex(soundex, name[0]);
+    processCharacters(name, soundex);
 }
 #endif
