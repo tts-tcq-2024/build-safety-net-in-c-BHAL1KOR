0c3c4cbdd930e9a3f085af7728c04af9670b0478
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-02 20:21:38 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,43 +1,56 @@
 #include "Soundex.h"
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
-  char soundex[MAX_CODE_LENGTH +1];
-  generateSoundex("Robert", soundex);
-  EXPECT_EQ(strcmp(soundex, "R163"),0);
-  
-  generateSoundex("Rupert", soundex);
-  EXPECT_EQ(strcmp(soundex, "R163"),0);
-  
-  generateSoundex("Rubin", soundex);
-  EXPECT_EQ(strcmp(soundex, "R150"),0);
-  
-  generateSoundex("Ashcraft", soundex);
-  EXPECT_EQ(strcmp(soundex, "A261"),0);
-  
-  generateSoundex("Ashcroft", soundex);
-  EXPECT_EQ(strcmp(soundex, "A261"),0);
-  
+	char soundex[MAX_CODE_LENGTH + 1];
+	generateSoundex("Robert", soundex);
+	EXPECT_EQ(strcmp(soundex, "R163"), 0);
+
+	generateSoundex("Rupert", soundex);
+	EXPECT_EQ(strcmp(soundex, "R163"), 0);
+
+	generateSoundex("Rubin", soundex);
+	EXPECT_EQ(strcmp(soundex, "R150"), 0);
+
+	generateSoundex("Ashcraft", soundex);
+	EXPECT_EQ(strcmp(soundex, "A261"), 0);
+
+	generateSoundex("Ashcroft", soundex);
+	EXPECT_EQ(strcmp(soundex, "A261"), 0);
+
 }
 TEST(SoudexTestsuite, TestingInvalidResultUsecases) {
-  char soundex[MAX_CODE_LENGTH +1];
-//"Tymczak" should yields "T522" not "T520"
-  generateSoundex("Tymczak", soundex);
-  EXPECT_NE(strcmp(soundex, "T522"),0);
+	char soundex[MAX_CODE_LENGTH + 1];
+	//"Tymczak" should yields "T522" not "T520"
+	generateSoundex("Tymczak", soundex);
+	EXPECT_NE(strcmp(soundex, "T522"), 0);
 
-  generateSoundex("Tymczak", soundex);
-  EXPECT_EQ(strcmp(soundex, "T520"),0);
+	generateSoundex("Tymczak", soundex);
+	EXPECT_EQ(strcmp(soundex, "T520"), 0);
 
-//"Pfister" should yields "P236" not "P123" 
-  generateSoundex("Pfister", soundex);
-  EXPECT_NE(strcmp(soundex, "P236"),0); 
+	//"Pfister" should yields "P236" not "P123" 
+	generateSoundex("Pfister", soundex);
+	EXPECT_NE(strcmp(soundex, "P236"), 0);
 
-  generateSoundex("Pfister", soundex);
-  EXPECT_EQ(strcmp(soundex, "P123"),0); 
+	generateSoundex("Pfister", soundex);
+	EXPECT_EQ(strcmp(soundex, "P123"), 0);
 
-// "Honeyman" should yields "H555".
-  generateSoundex("Honeyman", soundex);
-  EXPECT_NE(strcmp(soundex, "H555"),0);
+	// "Honeyman" should yields "H555".
+	generateSoundex("Honeyman", soundex);
+	EXPECT_NE(strcmp(soundex, "H555"), 0);
 
-  generateSoundex("Honeyman", soundex);
-  EXPECT_EQ(strcmp(soundex, "H500"),0);
+	generateSoundex("Honeyman", soundex);
+	EXPECT_EQ(strcmp(soundex, "H500"), 0);
+}
+
+TEST(SoudexTestsuite, TestForIntegres) {
+	char soundex[MAX_CODE_LENGTH + 1];
+	//"Tymczak" should yields "T522" not "T520"
+	generateSoundex("123456", soundex);
+	EXPECT_EQ(strcmp(soundex, ""), 0);
+
+	generateSoundex("", soundex);
+	EXPECT_EQ(strcmp(soundex, ""), 0);
+
+	generateSoundex(NULL, soundex);
+	EXPECT_EQ(strcmp(soundex, ""), 0);
 }
