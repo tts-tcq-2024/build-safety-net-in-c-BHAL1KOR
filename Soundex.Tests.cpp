5a5fa820a0484b83a03b2469af3a0c470385acb9
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-02 17:28:05 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,29 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA 
   char soundex[5];
   generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+  ASSERT_EQ(soundex,"A200");
+
+  generateSoundex("Robert", soundex);
+  ASSERT_EQ(soundex, "R163");
+
+  generateSoundex("Rupert", soundex);
+  ASSERT_EQ(soundex, "R163");
+
+  generateSoundex("Rubin", soundex);
+  ASSERT_EQ(soundex, "R150");
+
+  generateSoundex("Ashcraft", soundex);
+  ASSERT_EQ(soundex, "A261");
+
+  generateSoundex("Tymczak", soundex);
+  ASSERT_EQ(soundex, "T522");
+
+  generateSoundex("Pfister", soundex);
+  ASSERT_EQ(soundex, "P236");
+
+  generateSoundex("", soundex);
+  ASSERT_EQ(soundex, "");
+
+  generateSoundex(NULL, soundex);
+  ASSERT_EQ(soundex, "");
 }
