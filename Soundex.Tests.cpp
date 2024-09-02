c829de9cffad6194033fac7e806d7b54853ba202
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-02 19:51:33 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,7 +2,7 @@
 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
   char soundex[MAX_CODE_LENGTH +1];
-  generateSoundex("Robert", soundex)
+  generateSoundex("Robert", soundex);
   EXPECT_EQ(strcmp(soundex, "R163"),0);
 
 
