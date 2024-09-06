d6f46ceb1dc4d18d72182712d5f834a62a09c5ef
RoopashreeHT <BHAL1KOR@bosch.com>
2024-09-06 13:04:01 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,42 +1,67 @@
 #include "Soundex.h"
+char soundex[MAX_CODE_LENGTH + 1];
 
-TEST( SoudexTestsuite, EncodeCharacterWithEquivalentNumber )
-{
-    EXPECT_EQ(generateSoundex("Ashcraft"), "A261");
-    EXPECT_EQ(generateSoundex("Bach"), "B200");
-    EXPECT_EQ(generateSoundex("Pfister"), "P236");
-    EXPECT_EQ(generateSoundex("Honeyman"), "H555");
+TEST(SoudexTestsuite, EncodeCharacterWithEquivalentNumber) {  
+    generateSoundex("Ashcraft", soundex);
+    EXPECT_EQ(strcmp(soundex, "A261"), 0);
+    generateSoundex("Bach", soundex);
+    EXPECT_EQ(strcmp(soundex, "B200"), 0);
+    generateSoundex("Pfister", soundex);
+    EXPECT_EQ(strcmp(soundex, "P236"), 0);
+    generateSoundex("Honeyman", soundex);
+    EXPECT_EQ(strcmp(soundex, "H555"), 0);
 }
 
 TEST(SoudexTestsuite, DroppingOccurrencesOfVowelsAndSpecificConsonants) {
-    EXPECT_EQ(generateSoundex("Aeiouyhw"), "A000");
-    EXPECT_EQ(generateSoundex("Hello"), "H400");
-    EXPECT_EQ(generateSoundex("Soundex"), "S532");
-    EXPECT_EQ(generateSoundex("Algorithm"), "A426");
+    generateSoundex("Aeiouyhw", soundex);
+    EXPECT_EQ(strcmp(soundex, "A000"), 0);
+    generateSoundex("Hello", soundex);
+    EXPECT_EQ(strcmp(soundex, "H400"), 0);
+    generateSoundex("Soundex", soundex);
+    EXPECT_EQ(strcmp(soundex, "S532"), 0);
+    generateSoundex("Algorithm", soundex);
+    EXPECT_EQ(strcmp(soundex, "A426"), 0);
 }
 
 TEST(SoudexTestsuite, ReplacingConsonantsWithDigits) {
-    EXPECT_EQ(generateSoundex("Bfpv"), "B110");
-    EXPECT_EQ(generateSoundex("Cgjkqsxz"), "C222");
-    EXPECT_EQ(generateSoundex("Algorithm"), "A426");
-    EXPECT_EQ(generateSoundex("Soundex"), "S532");
+    char soundex[MAX_CODE_LENGTH + 1];
+    generateSoundex("Bfpv", soundex);
+    EXPECT_EQ(strcmp(soundex, "B110"), 0);
+    generateSoundex("Cgjkqsxz", soundex);
+    EXPECT_EQ(strcmp(soundex, "C222"), 0);
+    generateSoundex("Algorithm", soundex);
+    EXPECT_EQ(strcmp(soundex, "A426"), 0);
+    generateSoundex("Soundex", soundex);
+    EXPECT_EQ(strcmp(soundex, "S532"), 0);
 }
 
 TEST(SoundexTestsuite, SimilarSoundingWordsHaveSameSoundexOutput) {
-    EXPECT_EQ(generateSoundex("Robert"), generateSoundex("Rupert"));
-    EXPECT_EQ(generateSoundex("Rubin"), generateSoundex("Rabin"));
-    EXPECT_EQ(generateSoundex("Ashcraft"), generateSoundex("Ashcroft"));
-    EXPECT_EQ(generateSoundex("Jackson"), generateSoundex("Jaxen"));
+    generateSoundex("Robert", soundex);
+    EXPECT_EQ(strcmp(soundex, generateSoundex("Rupert")), 0);
+    generateSoundex("Rubin", soundex);
+    EXPECT_EQ(strcmp(soundex, generateSoundex("Rabin")), 0);
+    generateSoundex("Ashcraft", soundex);
+    EXPECT_EQ(strcmp(soundex, generateSoundex("Ashcroft")), 0);
+    generateSoundex("Jackson", soundex);
+    EXPECT_EQ(strcmp(soundex, generateSoundex("Jaxen")), 0);
 }
 
 TEST(SoundexTestsuite, EmptyInputReturnsEmptyOutput) {
-    EXPECT_EQ(generateSoundex(""), ""); 
-    EXPECT_EQ(generateSoundex(NULL), "");
+    generateSoundex("", soundex);
+    EXPECT_EQ(strcmp(soundex, ""), 0);
+    generateSoundex(NULL, soundex);
+    EXPECT_EQ(strcmp(soundex, ""), 0);
 }
 
+
 TEST(SoundexTestsuite, PadsWithZerosIfInputLessThanFourCharacters) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
-    EXPECT_EQ(generateSoundex("AB"), "A100");
-    EXPECT_EQ(generateSoundex("ABC"), "A120");
-    EXPECT_EQ(generateSoundex("D"), "D000");
+    generateSoundex("A", soundex);
+    EXPECT_EQ(strcmp(soundex, "A000"), 0);
+    generateSoundex("AB", soundex);
+    EXPECT_EQ(strcmp(soundex, "A100"), 0);
+    generateSoundex("ABC", soundex);
+    EXPECT_EQ(strcmp(soundex, "A120"), 0);
+    generateSoundex("D", soundex);
+    EXPECT_EQ(strcmp(soundex, "D000"), 0);
 }
+
